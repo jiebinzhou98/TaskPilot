d1823659ddbb9524580b0cbaf5f04721bb1ef216
jiebinzhou98 <benzhou98@outlook.com>
2025-05-13 15:58:39 -0400

    fixing the display UI for the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,9 +22,16 @@ export default function DashboardPage() {
   }, [router]);
 
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <p>Current User: {uid}</p>
+    <main className="min-h-screen bg-gray-100 p-8">
+        <div className='max-w-3xl mx-auto bg-white rounded-2xl shadow-md p-6'>
+            <h1 className='text-3xl font-bold mb-4'>🔥Welcome to TaskPilot🔥</h1>
+            <p className='text-gray-700 mb-2'>Login As Anonymous</p>
+            <p className='text-sm text-gray-500 break-all'>User: <span className='font-mono'>{uid}</span></p>
+            
+            <div className='mt-8 border-t pt-4 text-gray-400 italic'>
+                You dont have any tasks yet, Click to begin!
+            </div>
+        </div>
     </main>
   );
 }
