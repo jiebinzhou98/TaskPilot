51476b5446fd48e53065f39b49d3f60f532dd0de
jiebinzhou98 <benzhou98@outlook.com>
2025-05-13 12:01:29 -0400

    initial set up for the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-green-600">TaskPilot 🚀</h1>
+      </main>
+    </div>
+  );
+}
